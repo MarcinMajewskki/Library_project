9e2750cbf89dc3018008d553d5d70b8558a92f0c
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2023-01-14 15:28:16 +0100

    database working

--- src/main/java/org/example/library/Main.java
+++ src/main/java/org/example/library/Main.java
@@ -1,19 +1,12 @@
 package org.example.library;
 
-import org.example.library.model.User;
-import org.example.library.utils.HibernateUtil;
 import org.example.library.view.LoginView;
 import org.example.library.view.View;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 
 import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.close();
         View view = new LoginView(Optional.empty());
         view.display();
     }
