1a50d9228e886651a8fe6b4c4a8e321b5e384ec1
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2023-01-14 10:35:28 +0100

    added User class

--- src/main/java/org/example/library/Main.java
+++ src/main/java/org/example/library/Main.java
@@ -1,7 +1,10 @@
 package org.example.library;
 
+import org.example.library.model.User;
+
 public class Main {
     public static void main(String[] args) {
+        User user = new User(1, "login", "pass", "name");
         String testCheckstyle = "test";
         System.out.println(testCheckstyle);
     }
