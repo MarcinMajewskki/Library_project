f8d96d8d6b7d0e302572da5e2d7a456032803145
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2023-01-14 10:09:30 +0100

    first config

---
+++ src/main/java/org/example/library/Main.java
@@ -0,0 +1,8 @@
+package org.example.library;
+
+public class Main {
+    public static void main(String[] args) {
+        String testCheckstyle = "test";
+        System.out.println(testCheckstyle);
+    }
+}
