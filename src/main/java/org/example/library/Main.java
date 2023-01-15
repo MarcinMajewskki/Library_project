6175aef78c1a2168cdbb2bcfc33894a5c19c78a0
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2023-01-15 13:44:38 +0100

    unecesary code removal

--- src/main/java/org/example/library/Main.java
+++ src/main/java/org/example/library/Main.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        UserRole userRole = UserRole.find("USER");
-        System.out.println(userRole.USER.getName());
         View view = new LoginView(Optional.empty());
         view.display();
     }
