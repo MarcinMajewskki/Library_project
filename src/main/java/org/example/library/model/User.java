3ee4fb39f2f56728a434205cae4b91116cf5d7ca
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2023-01-15 10:48:28 +0100

    user role

--- src/main/java/org/example/library/model/User.java
+++ src/main/java/org/example/library/model/User.java
@@ -3,13 +3,16 @@ package org.example.library.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @Entity
 @NoArgsConstructor
+@ToString(exclude = {"roles","address"})
 public class User {
 
     @Id
@@ -18,5 +21,17 @@ public class User {
     private String login;
     private String password;
     private String name;
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "User_Role",
+            joinColumns = { @JoinColumn(name = "user_id")},
+            inverseJoinColumns = { @JoinColumn(name = "role_id")}
+    )
+    private List<Role> roles;
+
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private Address address;
 
 }
