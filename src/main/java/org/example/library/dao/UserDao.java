116723d13633f3e554d3bf1a10b5fd77eb0a78c5
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2023-01-15 09:19:58 +0100

    checkstyle fix

--- src/main/java/org/example/library/dao/UserDao.java
+++ src/main/java/org/example/library/dao/UserDao.java
@@ -9,12 +9,12 @@ import java.util.Optional;
 
 public class UserDao implements UserProvider {
 
-    private final String SEARCH_BY_LOGIN_QUERY = "from User u where u.login=:login";
+    private static final String SEARCH_BY_LOGIN_QUERY = "from User u where u.login=:login";
 
     @Override
     public Optional<User> findUserByLogin(String login) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        Query<User> query = session.createQuery(SEARCH_BY_LOGIN_QUERY,User.class)
+        Query<User> query = session.createQuery(SEARCH_BY_LOGIN_QUERY, User.class)
                 .setParameter("login", login);
         Optional<User> user = query.getResultList().stream().findFirst();
         session.close();
